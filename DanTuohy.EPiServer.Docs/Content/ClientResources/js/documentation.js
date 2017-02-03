88c7666db52c7700a9550cd087f44ae322bc4a5a
Dan Tuohy <dantuohy@gmail.com>
2017-02-03 21:55:15 +1100

    added alloy site

---
+++ DanTuohy.EPiServer.Docs/Content/ClientResources/js/documentation.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+  $('.collapsible').collapsible();
+
+  initExpandCollapse();
+
+  function initExpandCollapse() {
+    var $collapsible = $(".document .collapsible");
+
+    $(".document .expander")
+      .on("click",
+        function() {
+          var $self = $(this);
+          if (!$self.hasClass("expanded")) {
+            $(".document .collapsible .collapsible-header").addClass("active");
+            $collapsible.collapsible({ accordion: false });
+            $self.addClass("expanded");
+          } else {
+            $self.removeClass("expanded");
+            $(".document .collapsible .collapsible-header").removeClass(function () {
+              return "active";
+            });
+            $collapsible.collapsible({ accordion: true });
+            $collapsible.collapsible({ accordion: false });
+          }
+        });
+  }
+});
---
+++ Test8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Test8.MvcApplication" Language="C#" %>
