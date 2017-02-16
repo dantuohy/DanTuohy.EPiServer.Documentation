c835b1dac5f06899da7e9a64b7fdcaa576bd2e3a
AP-MEDIA\dan.tuohy <dan.tuohy@isobar.com>
2017-02-16 17:07:45 +1100

    Added new content type addon

---
+++ Test9/modules/Tuohy.Epi.Docs/ClientResources/js/inUse.js
@@ -0,0 +1,56 @@
+﻿(function () {
+  // using this pattern helps keeping your privates private
+  inUse = {};
+  inUse.init = function () {
+    var $container = $(".in-use");
+
+    $(".content-type a", $container).click(
+      function () {
+        var url = "inuse/GetLinks/" + $(this).attr("data-id");
+        
+        getLinks($(this).attr("data-id"));
+
+      });
+
+    $(".paging a").click(function() {
+      var pageNumber = $("#pageNumber").val();
+      var id = $("#id").val();
+      if ($(this).hasClass("previous")) {
+
+        pageNumber--;
+      } else {
+        pageNumber++;
+      }
+      getLinks(id, pageNumber);
+    });
+
+    $("#back").click(function() {
+      $.ajax({
+        type: "get",
+        url: "inuse/index/",
+        success: function (view) {
+          $container.html(view);
+        }
+      });
+    });
+
+    function getLinks(id, pageNumber) {
+
+      var url = "inuse/GetLinks/" + id;
+
+      if (pageNumber != null) {
+        url = url + "?page=" + pageNumber;
+      }
+
+      $.ajax({
+        type: "get",
+        url: url,
+        success: function (view) {
+          $container.html(view);
+        }
+      });
+    }
+  };
+
+
+})();
---
+++ Tuohy.Epi.ContentTypesInUse/Models/ContentTypeCountsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Tuohy.Epi.ContentTypesInUse.Models
+{
+    public class ContentTypeCountsViewModel
+    {
+        public int ContentTypeId { get; set; }
+        public string ContentTypeName { get; set; }
+        public int Count { get; set; }
+    }
+
+}
