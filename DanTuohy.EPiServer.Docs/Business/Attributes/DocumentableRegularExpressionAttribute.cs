701d1e261413906188787cf73e1d85e0c3f6cffb
Dan Tuohy <dantuohy@gmail.com>
2017-07-22 13:14:26 +1000

    Adding documentation images

--- DanTuohy.EPiServer.Docs/Business/Attributes/DocumentableRegularExpressionAttribute.cs
+++ DanTuohy.EPiServer.Docs/Business/Attributes/DocumentableRegularExpressionAttribute.cs
@@ -4,7 +4,7 @@ using Tuohy.Epi.Docs.Interfaces;
 namespace Tuohy.Epi.Docs.Business.Attributes
 {
     /// <summary>
-    /// Regualr expression attribute that extends <see cref="IDocumentable"></see>/>
+    /// Regular expression attribute that extends <see cref="IDocumentable"></see>/>
     /// </summary>
     public class DocumentableRegularExpressionAttribute : RegularExpressionAttribute, IDocumentable
     {
---
+++ Test10/modules/Tuohy.Epi.Docs/ClientResources/js/documentation.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+  $('.collapsible').collapsible();
+
+  initExpandCollapse();
+
+  $(".content-types .collapsible-body ul li.active").goTo(".content-types");
+
+  function initExpandCollapse() {
+    var $collapsible = $(".document .collapsible");
+
+    $(".document .expander")
+      .on("click",
+        function() {
+          var $self = $(this);
+          if (!$self.hasClass("expanded")) {
+            $(".document .collapsible .collapsible-header").addClass("active");
+            $collapsible.collapsible({ accordion: false });
+            $self.addClass("expanded");
+          } else {
+            $self.removeClass("expanded");
+            $(".document .collapsible .collapsible-header").removeClass(function () {
+              return "active";
+            });
+            $collapsible.collapsible({ accordion: true });
+            $collapsible.collapsible({ accordion: false });
+          }
+        });
+  }
+});
+
+(function ($) {
+  $.fn.goTo = function (selector) {
+    $(selector).animate({
+      scrollTop: $(this).offset().top + 'px'
+    }, 'fast');
+    return this;
+  }
+})(jQuery);
